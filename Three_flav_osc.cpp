9a6364a69c42f2bfc56d25b1cb5e7eee2f8c0f23
Daniel Forero <df.forero10@uniandes.edu.co>
2017-01-26 18:46:43 -0500

    Issues with eigenvalues

--- Three_flav_osc.cpp
+++ Three_flav_osc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_matrix.h>
@@ -6,6 +7,9 @@ using namespace std;
 #include <complex>
 #include <cmath>
 #include <gsl/gsl_sf_trig.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_eigen.h>
+#include <gsl/gsl_vector.h>
 /* This program should be compiled as:
 g++ -o oscillations Three_flav_osc.cpp `gsl-config --cflags --libs`
 */
@@ -15,11 +19,11 @@ long double A;
 double m_N=939.57 ; //MeV
 
 //Define matrix elements
-gsl_complex Ue1, Ue2, Ue3, Umu1, Umu2, Umu3, Ut1, Ut2, Ut3;
-gsl_complex E1, E2, E3;
+double Ue1, Ue2, Ue3, Umu1, Umu2, Umu3, Ut1, Ut2, Ut3;
+double E12, E13, E21, E23, E31, E32;
 //Define squared mass differences.
-float dM32 = 3.2 * pow(10, -3); //eV^2
-float dm21 = 0; //eV^2
+double dM32 = 3.2 * pow(10, -3); //eV^2
+double dm21 = 0; //eV^2
 
 //Define vacuum mixing angles.
 int theta1 = 45; //Degrees
@@ -30,10 +34,28 @@ double delta = 0;
 
 //Distance
 float L=1;
-int main(){
 
+
+int main(){
+clock_t t1,t2;
+t1=clock();
+
+E12=-E21;
+E23=-E32;
+E31=-E12-E23;
+E13=-E31;
 double density( double lon);
 //Calculate matrix elements.
+Ue1 = gsl_sf_cos(theta2)*gsl_sf_cos(theta3);
+Ue2 = gsl_sf_sin(theta3)*gsl_sf_cos(theta2);
+Ue3 = gsl_sf_sin(theta2);
+Umu1=-gsl_sf_sin(theta3)*gsl_sf_cos(theta1)-gsl_sf_sin(theta1)*gsl_sf_sin(theta2)*gsl_sf_cos(theta3);
+Umu2=gsl_sf_cos(theta1)*gsl_sf_cos(theta3)-gsl_sf_sin(theta1)*gsl_sf_sin(theta2)*gsl_sf_sin(theta3);
+Umu3=gsl_sf_sin(theta1)*gsl_sf_cos(theta2);
+Ut1=gsl_sf_sin(theta1)*gsl_sf_sin(theta3)-gsl_sf_sin(theta2)*gsl_sf_cos(theta2)*gsl_sf_cos(theta3);
+Ut2=-gsl_sf_sin(theta1)*gsl_sf_cos(theta3)-gsl_sf_sin(theta2)*gsl_sf_sin(theta3)*gsl_sf_cos(theta1);
+Ut3=gsl_sf_cos(theta1)*gsl_sf_cos(theta2);
+/*
 gsl_complex phaseCKM = gsl_complex_polar(1, delta); 
 Ue1=gsl_complex_rect(gsl_sf_cos(theta2)*gsl_sf_cos(theta3), 0);
 Ue2=gsl_complex_rect(gsl_sf_sin(theta3)*gsl_sf_cos(theta2), 0);
@@ -44,43 +66,90 @@ Umu3=gsl_complex_rect(gsl_sf_sin(theta1)*gsl_sf_cos(theta2), 0);
 Ut1=gsl_complex_add(gsl_complex_rect(gsl_sf_sin(theta1)*gsl_sf_sin(theta3), 0), gsl_complex_mul(gsl_complex_rect(-gsl_sf_sin(theta2)*gsl_sf_cos(theta1)*gsl_sf_cos(theta3), 0), phaseCKM));
 Ut2=gsl_complex_add(gsl_complex_rect(-gsl_sf_sin(theta1)*gsl_sf_cos(theta3), 0), gsl_complex_mul(gsl_complex_rect(-gsl_sf_sin(theta2)*gsl_sf_sin(theta3)*gsl_sf_cos(theta1), 0), phaseCKM));
 Ut3=gsl_complex_rect(gsl_sf_cos(theta1)*gsl_sf_cos(theta2), 0);
-
-cout << "Matrix elements calculated, phase = " << delta << endl;
+*/
+cout << "Matrix elements calculated" << endl;
 
 //Build CKM matrix.
-gsl_matrix_complex *CKM = gsl_matrix_complex_alloc(3, 3);
-gsl_matrix_complex_set (CKM, 0, 0, Ue1);
-gsl_matrix_complex_set (CKM, 0, 1, Ue2);
-gsl_matrix_complex_set (CKM, 0, 2, Ue3);
-gsl_matrix_complex_set (CKM, 1, 0, Umu1);
-gsl_matrix_complex_set (CKM, 1, 1, Umu2);
-gsl_matrix_complex_set (CKM, 1, 2, Umu3);
-gsl_matrix_complex_set (CKM, 2, 0, Ut1);
-gsl_matrix_complex_set (CKM, 2, 1, Ut2);
-gsl_matrix_complex_set (CKM, 2, 2, Ut3); 
+
+gsl_matrix *CKM = gsl_matrix_alloc(3, 3);
+
+gsl_matrix_set (CKM, 0, 0, Ue1);
+gsl_matrix_set (CKM, 0, 1, Ue2);
+gsl_matrix_set (CKM, 0, 2, Ue3);
+gsl_matrix_set (CKM, 1, 0, Umu1);
+gsl_matrix_set (CKM, 1, 1, Umu2);
+gsl_matrix_set (CKM, 1, 2, Umu3);
+gsl_matrix_set (CKM, 2, 0, Ut1);
+gsl_matrix_set (CKM, 2, 1, Ut2);
+gsl_matrix_set (CKM, 2, 2, Ut3); 
+
 cout << "CKM matrix built..." << endl;
 
 
 //Invert CKM matrix.
-gsl_matrix_complex *ICKM = gsl_matrix_complex_alloc(3, 3);
-gsl_matrix_complex_transpose_memcpy(ICKM, CKM);
+gsl_matrix *ICKM = gsl_matrix_alloc(3, 3);
+gsl_matrix_transpose_memcpy(ICKM, CKM);
 cout << "CKM matrix inverted (transposed)..." << endl;
 
-//Build Hamiltonian in mass basis
-gsl_matrix_complex *H_m = gsl_matrix_complex_alloc(3, 3);
-gsl_matrix_complex_set_zero(H_m);
+
+//Build perturbation in flavor basis
+gsl_matrix *V_f = gsl_matrix_alloc(3, 3);
+gsl_matrix_set_zero(V_f);
+gsl_matrix_set (V_f, 0, 0, 1.0); 
+//Build perturbation in mass basis
+//V_m=U^-1 V_f U
+
+gsl_matrix *V_m = gsl_matrix_alloc(3, 3);
+gsl_matrix *first = gsl_matrix_alloc(3, 3);
+gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, ICKM, V_f, 0.0, first);
+gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, first, CKM, 0.0, V_m); 
+gsl_matrix_free(first);
+
 //Matter density A
 A=(1/sqrt(2))*G_f*(1/(m_N*pow(10, -3)))*density(L);
-
+gsl_matrix_scale(V_m, A);
 cout << "The matter density is " << A << endl;
 //Build matrix T
-gsl_matrix_complex *T = gsl_matrix_complex_alloc(3, 3);
+gsl_matrix *T = gsl_matrix_alloc(3, 3);
+gsl_matrix *traceHm = gsl_matrix_alloc(3, 3);
+//Matrix traceHm is defined as H_m - 1/3trH_m.
+gsl_matrix_set_zero(traceHm);
+gsl_matrix_set(traceHm, 0, 0, (1/3)*(E12+E13-A));
+gsl_matrix_set(traceHm, 1, 1, (1/3)*(E21+E23-A));
+gsl_matrix_set(traceHm, 2, 2, (1/3)*(E31+E32-A));
+gsl_matrix_add(V_m, traceHm);
+gsl_matrix *otherT = gsl_matrix_alloc(3, 3);
+gsl_matrix_memcpy(V_m, T);
+gsl_matrix_memcpy(V_m, otherT);
+gsl_matrix_free(V_m);
+gsl_matrix_free(traceHm);
+//Calculate the eigenvalues of T
+gsl_eigen_nonsymm_workspace *w = gsl_eigen_nonsymm_alloc(3);
+
+gsl_vector_complex *eigval = gsl_vector_complex_alloc(3);
+gsl_eigen_nonsymm(otherT, eigval, w);
+for (int i = 0; i<3;i++){
+	gsl_complex eval_i = gsl_vector_complex_get (eigval, i);
+	double eival_i=gsl_complex_abs(eval_i);
+	cout << "lambda " << eival_i << endl;
+}
+
+t2=clock();
+float diff ((float)t2-(float)t1);
+cout<<"Time elapsed " << diff << endl;
+
 
 return 0;
 }
 
 double density ( double lon){
 
-return 1;
+return 1.0;
 
+}
+
+double energies ( double massq, double neut_energy){
+
+double Eij = massq/(2*neut_energy);
+return Eij;
 }
